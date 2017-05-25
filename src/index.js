9a868b241370c7e10a58640bf01fe420e5337d1e
Hollis Lau <hollislau@hotmail.com>
2017-05-24 22:23:46 -0700

    Remove unnecessary boilerplate

--- src/index.js
+++ src/index.js
@@ -4,5 +4,11 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
+if (module.hot) module.hot.accept();
+
 registerServiceWorker();
