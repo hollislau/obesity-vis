8847fc49583b04fe1f33e76c23d89f6dcb5797ec
Hollis Lau <hollislau@hotmail.com>
2017-05-25 02:02:02 -0700

    Add data fetching functionality

--- src/components/Chart/index.js
+++ src/components/Chart/index.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import * as d3 from 'd3';
 import Histogram from '../Histogram';
 
+const styles = {
+  width: 500,
+  height: 300,
+  padding: 30
+};
+
 class Chart extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: null
+    };
+  }
+
+  fetchData() {
+    d3.csv('./obesity_us_data.csv', (err, data) => {
+      if (err) console.log(`Unable to fetch data: ${ err }`);
+
+      this.setState({ data });
+    });
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
   render() {
+    const { data } = this.state;
+
     return (
-      <Histogram />
+      data && <Histogram />
     );
   }
 }
