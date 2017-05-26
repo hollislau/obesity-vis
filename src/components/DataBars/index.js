6d2ae183f83a4555e960142e52c3b1583b810a9b
Hollis Lau <hollislau@hotmail.com>
2017-05-26 03:19:22 -0700

    Refactor to use destructuring in function signatures

--- src/components/DataBars/index.js
+++ src/components/DataBars/index.js
@@ -1,24 +1,21 @@
 import React from 'react';
 import './index.css';
 
-const renderBars = props => (d, i) => {
-  const { height, padding, xScale, yScale } = props;
-  const barProps = {
-    className: 'bar',
-    x: xScale(d.age_start),
-    y: yScale(d.mean),
-    width: xScale.bandwidth(),
-    height: height - padding - yScale(d.mean),
-    key: i
+const renderBars = ({ height, padding, xScale, yScale }) =>
+  (d, i) => {
+    const barProps = {
+      className: 'bar',
+      x: xScale(d.age_start),
+      y: yScale(d.mean),
+      width: xScale.bandwidth(),
+      height: height - padding - yScale(d.mean),
+      key: i
+    };
+
+    return <rect { ...barProps } />;
   };
 
-  return <rect { ...barProps } />;
-};
-
-const DataBars = props => {
-  return (
-    <g>{ props.chartData.map(renderBars(props)) }</g>
-  );
-};
+const DataBars = props =>
+  <g>{ props.chartData.map(renderBars(props)) }</g>;
 
 export default DataBars;
--- src/components/Histogram/index.js
+++ src/components/Histogram/index.js
@@ -4,28 +4,22 @@ import DataBars from '../DataBars';
 
 const yMax = data => d3.max(data, d => d.mean);
 
-const xScale = props => {
-  const { chartData, width, padding } = props;
-
-  return d3.scaleBand()
+const xScale = ({ chartData, width, padding }) =>
+  d3.scaleBand()
     .domain(chartData.map(d => d.age_start))
     .rangeRound([padding, width - padding])
     .padding(0.1);
-};
 
-const yScale = props => {
-  const { chartData, height, padding } = props;
-
-  return d3.scaleLinear()
+const yScale = ({ chartData, height, padding }) =>
+  d3.scaleLinear()
     .domain([0, yMax(chartData)])
     .range([height - padding, padding]);
-};
 
 const Histogram = props => {
   const { width, height } = props;
   const scales = {
     xScale: xScale(props),
-    yScale: yScale(props),
+    yScale: yScale(props)
   };
 
   return (
