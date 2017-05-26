57f5f90540fb9578ad1acbb6b0511da969104217
Hollis Lau <hollislau@hotmail.com>
2017-05-26 13:43:03 -0700

    Apply settings to x and y axes

--- src/components/XYAxis/index.js
+++ src/components/XYAxis/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import * as d3 from 'd3';
 import Axis from '../Axis';
 
-const XYAxis = props =>
-  <g>
-    <Axis />
-  </g>;
+const XYAxis = ({ height, padding, xScale, yScale }) => {
+  const xSettings = {
+    translate: `translate(0, ${ height - padding })`,
+    scale: xScale,
+    orient: 'bottom'
+  };
+  const ySettings = {
+    translate: `translate(${ padding }, 0)`,
+    scale: yScale,
+    orient: 'left'
+  };
+
+  return (
+    <g>
+      <Axis { ...xSettings }/>
+      <Axis { ...ySettings }/>
+    </g>
+  );
+};
 
 export default XYAxis;
